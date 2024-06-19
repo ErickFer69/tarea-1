981471a768e949e50fd8869d658305e2df0877ac
ErickFer69 <erickcarrenov.est@uens.edu.ec>
2024-06-19 17:40:37 +0000

    Pending changes exported from your codespace

--- apperick/models.py
+++ apperick/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Estudiante(models.Model):
+    nombre = models.CharField(max_length=200)
+    fecha_nacimiento = models.DateTimeField()
